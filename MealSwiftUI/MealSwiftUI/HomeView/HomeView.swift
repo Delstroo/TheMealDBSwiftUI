13c66acd1fa091ba75f7cdadb774795e98e3b016
Delstroo <delstunmccray@gmail.com>
2023-09-04 11:39:44 -0700

    this adds a bottom view that will act as a navigation view for the app

--- MealSwiftUI/MealSwiftUI/HomeView/HomeView.swift
+++ MealSwiftUI/MealSwiftUI/HomeView/HomeView.swift
@@ -20,123 +20,177 @@ struct HomeView: View {
     
     var animation: Namespace.ID
     @State private var isLoginViewPresented = false
+    @State private var isToolbarVisible = true
+    @State private var scrollOffset: CGFloat = 0
+    @State private var lastContentOffset: CGFloat = 0
 
+    
     var body: some View {
-        NavigationStack {
-            ScrollView {
-                HStack(spacing: 12) {
-                    Button {
-
-                    } label: {
-                        Image(systemName: "line.3.horizontal.decrease")
-                    }
-                    .frame(width: 60, height: 60)
-                    .background(Color(uiColor: .secondarySystemFill))
-                    .foregroundColor(.primary)
-                    .cornerRadius(15)
-
-                    Spacer() // This will push the following button to the trailing edge
-
-                    Button {
-                        isLoginViewPresented.toggle()
-                    } label: {
-                        Image(systemName: "person.fill")
-                    }
-                    .frame(width: 60, height: 60)
-                    .background(Color(uiColor: .secondarySystemFill))
-                    .foregroundColor(.primary)
-                    .cornerRadius(15)
-                }
-                .frame(maxWidth: .infinity) // This makes the HStack fill the width of the screen
-                .padding(.horizontal, 20)
-                TopHomeView()
-                HStack(spacing: 12, content: {
-                    CustomSearchBar(searchText: searchTextBinding)
-                        .onTapGesture {
-                            homeVM.isSearchTapped.toggle()
-                        }
-                        .padding(.horizontal, 20)
-                        .padding(.bottom, 40)
-                })
-                
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        
-                        ForEach(MealService.CategoryMealSort.allCases, id: \.self) { category in
-                            HStack {
-                                Text(category.rawValue)
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(mealService.categorySelected != category.rawValue ? Color(UIColor.secondaryLabel.withAlphaComponent(0.45)) : Color(.label))
-                                    .padding(.vertical, 8)
-                                    .padding(.leading, 12)
-                                    .frame(maxWidth: .infinity)
+        NavigationView {
+            GeometryReader { geometry in
+                ScrollViewReader { scrollProxy in
+                    ScrollView {
+                        HStack(spacing: 12) {
+                            Button {
                                 
-                                Image(category.rawValue)
-                                    .resizable()
-                                    .frame(width: 25, height: 25)
-                                    .padding(.trailing, 12)
-//                                    .padding(.leading, 6)
+                            } label: {
+                                Image(systemName: "line.3.horizontal.decrease")
                             }
-                                .background {
-                                    if mealService.categorySelected == category.rawValue {
-                                        Capsule()
-                                            .fill(Color("sliderColor").opacity(0.55))
-                                            .shadow(color: .black.opacity(0.1), radius: 5, x: 5, y: 5)
-                                            .matchedGeometryEffect(id: "MENU", in: animation)
-                                    }
-                                }
-                                .onTapGesture {
-                                    withAnimation(.easeInOut){
-                                        mealService.categorySelected = category.rawValue
-                                        homeVM.search = ""
-                                        
-                                    }
-                                }
+                            .frame(width: 60, height: 60)
+                            .background(Color(uiColor: .secondarySystemFill))
+                            .foregroundColor(.primary)
+                            .cornerRadius(15)
+                            
+                            Spacer() // This will push the following button to the trailing edge
+                            
+                            Button {
+                                isLoginViewPresented.toggle()
+                            } label: {
+                                Image(systemName: "person.fill")
+                            }
+                            .frame(width: 60, height: 60)
+                            .background(Color(uiColor: .secondarySystemFill))
+                            .foregroundColor(.primary)
+                            .cornerRadius(15)
                         }
-                    }//:Hstack
-                    .padding(.horizontal, 12)
-                }//:ScrollView
-                
-                ScrollView(.vertical, showsIndicators: false) {
-                    LazyVGrid(columns: [
-                        GridItem(.flexible()), // You can adjust .flexible() as needed
-                        GridItem(.flexible())
-                    ]) {
-                        ForEach(mealService.mealCategory, id: \.self) { meals in
-                            NavigationLink(value: meals) {
-                                CategoryView(category: meals)
+                        .frame(maxWidth: .infinity) // This makes the HStack fill the width of the screen
+                        .padding(.horizontal, 20)
+                        TopHomeView()
+                        HStack(spacing: 12, content: {
+                            CustomSearchBar(searchText: searchTextBinding)
+                                .onTapGesture {
+                                    homeVM.isSearchTapped.toggle()
+                                }
+                                .padding(.horizontal, 20)
+                                .padding(.bottom, 40)
+                        })
+                        
+                        
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack {
+                                
+                                ForEach(MealService.CategoryMealSort.allCases, id: \.self) { category in
+                                    HStack {
+                                        Text(category.rawValue)
+                                            .fontWeight(.semibold)
+                                            .foregroundColor(mealService.categorySelected != category.rawValue ? Color(UIColor.secondaryLabel.withAlphaComponent(0.45)) : Color(.label))
+                                            .padding(.vertical, 8)
+                                            .padding(.leading, 12)
+                                            .frame(maxWidth: .infinity)
+                                        
+                                        Image(category.rawValue)
+                                            .resizable()
+                                            .frame(width: 25, height: 25)
+                                            .padding(.trailing, 12)
+                                        //                                    .padding(.leading, 6)
+                                    }
+                                    .background {
+                                        if mealService.categorySelected == category.rawValue {
+                                            Capsule()
+                                                .fill(Color("sliderColor").opacity(0.55))
+                                                .shadow(color: .black.opacity(0.1), radius: 5, x: 5, y: 5)
+                                                .matchedGeometryEffect(id: "MENU", in: animation)
+                                        }
+                                    }
+                                    .onTapGesture {
+                                        withAnimation(.easeInOut){
+                                            mealService.categorySelected = category.rawValue
+                                            homeVM.search = ""
+                                            
+                                        }
+                                    }
+                                }
+                            }//:Hstack
+                            .padding(.horizontal, 12)
+                        }//:ScrollView
+                        
+                        ScrollView(.vertical, showsIndicators: false) {
+                            LazyVGrid(columns: [
+                                GridItem(.flexible()), // You can adjust .flexible() as needed
+                                GridItem(.flexible())
+                            ]) {
+                                ForEach(mealService.mealCategory, id: \.self) { meals in
+                                    NavigationLink(value: meals) {
+                                        CategoryView(category: meals)
+                                    }
+                                }
                             }
+                        }
+                        .navigationDestination(for: Categories.self) { meal in
+                            
+                            MealDetailView(detailVM: DetailViewModel(categoryDescription: meal))
                         }
                     }
+                    .background(Color("bgColor"))
+                    .overlay(
+                        VStack {
+                            if isToolbarVisible {
+                                BottomToolbarView(
+                                    homeAction: {
+                                        // Action for the Home button
+                                    },
+                                    addRecipeAction: {
+                                        // Action for the Add Recipe button
+                                    },
+                                    accountAction: {
+                                        isLoginViewPresented.toggle()
+                                    }
+                                )
+                                .transition(.move(edge: .bottom))
+                                .animation(.easeInOut)
+                            }
+                        }
+                        .frame(width: geometry.size.width, alignment: .bottom)
+                        , alignment: .bottom
+                    )
+                    .onChange(of: mealService.categorySelected, perform: { newCategory in
+                        Task {
+                            await mealService.fetchMealCategory(category: newCategory)
+                        }
+                    })
+                    .task {
+                        await mealService.fetchMealCategory(category: mealService.categorySelected)
+                    }
+                    .navigationBarTitle("", displayMode: .inline)
+                    .navigationBarHidden(true)
+                    .onAppear {
+                        // Hide the toolbar initially
+                        isToolbarVisible = true
+                    }
+                    .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)) { _ in
+                        // Hide the toolbar when the keyboard is shown
+                        withAnimation {
+                            isToolbarVisible = false
+                        }
+                    }
+                    .onReceive(NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)) { _ in
+                        // Show the toolbar when the keyboard is hidden
+                        withAnimation {
+                            isToolbarVisible = true
+                        }
+                    }
+                    .onAppear {
+                        // Scroll to the top initially
+                        withAnimation {
+                            scrollProxy.scrollTo(0, anchor: .top)
+                        }
+                    }
+                    .simultaneousGesture(
+                        DragGesture().onChanged { value in
+                            let currentContentOffset = value.translation.height
+                            if currentContentOffset < 0 {
+                                // Scrolling down
+                                isToolbarVisible = false
+                            } else {
+                                // Scrolling up
+                                isToolbarVisible = true
+                            }
+                            lastContentOffset = currentContentOffset
+                        }
+                    )
                 }
-                .navigationDestination(for: Categories.self) { meal in
-                    
-                    MealDetailView(detailVM: DetailViewModel(categoryDescription: meal))
-                }
-            }
-            .background(Color("bgColor"))
-        }//: NavigationStack
-        .fullScreenCover(isPresented: $homeVM.isSearchTapped, content: {
-            NavigationStack {
-                MealSearchView()
-            }
-        })//: FullScreenCover
-        .sheet(isPresented: $isLoginViewPresented) {
-            NavigationView {
-                LoginView()
             }
         }
-        
-        .onChange(of: mealService.categorySelected, perform: { newCategory in
-            Task{
-                await mealService.fetchMealCategory(category: newCategory)
-            }//: Task
-            
-        })//: onChange
-        .task {
-            await mealService.fetchMealCategory(category: mealService.categorySelected)
-        }//: Task
     }
 }
 
@@ -149,3 +203,10 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+struct ScrollDirectionPreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
